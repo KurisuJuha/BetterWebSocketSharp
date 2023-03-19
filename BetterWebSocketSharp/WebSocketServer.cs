f2b40c7c6acdc37fbe74b58955b186967874aafd
KYapp <kukiyomenaisapp@gmail.com>
2023-03-19 19:16:33 +0900

    何か分からないけどとりあえずコミット

---
+++ BetterWebSocketSharp/WebSocketServer.cs
@@ -0,0 +1,16 @@
+﻿using WebSocketSharpServer = WebSocketSharp.Server.WebSocketServer;
+
+namespace JuhaKurisu.BetterWebSocketSharp;
+
+public class WebSocketServer
+{
+    public readonly int port;
+    private WebSocketSharpServer sharpServer;
+
+    public WebSocketServer(int port)
+    {
+        this.port = port;
+        sharpServer = new WebSocketSharpServer(port);
+        sharpServer.AllowForwardedRequest = true;
+    }
+}
